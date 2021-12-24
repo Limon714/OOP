17c2de539f643f2df3ecd4f7b02b5bdd4ec88f24
Abdul Khalaque <72975868+Limon714@users.noreply.github.com>
2021-12-24 21:20:49 +0600

    Create constructor.php

---
+++ constructor.php
@@ -0,0 +1,13 @@
+<?php
+
+// Object Oriented programming
+class Student{
+    public function __construct($name)
+    {
+        echo "Hello," .$name;
+    }
+}
+
+$ob = new Student("Limon");
+
+ ?>
